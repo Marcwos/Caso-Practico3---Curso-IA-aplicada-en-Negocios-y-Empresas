f449662ecb5c4c585a8da4c7edab5c0f13cf4682
Marcwos <ubituna22@gmail.com>
2025-05-25 16:31:11 -0500

    Configuracion de parametros

--- Maximizacion.py
+++ Maximizacion.py
@@ -1,2 +1,13 @@
 #Inteligencia Artificial aplicada a Negocios y Empresas
 #Maximización de beneficios de una empresa de venta online con Muestreo Thompson
+
+#Imporptar librerias
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Configuracion de parametros
+
+N = 10000
+d = 9
+
