146f0526cf0491e1ff3922871256446691894cb8
Marcwos <ubituna22@gmail.com>
2025-06-03 11:39:48 -0500

    Final de seccio3 Maximizacion

--- Maximizacion.py
+++ Maximizacion.py
@@ -9,11 +9,11 @@ import random
 
 #Configuracion de parametros
 
-N = 10000
+N = 500
 d = 9
 
 #Creacion de la simulacion
-conversion_rates = [0.05, 0.13, 0.9, 0.16, 0.11, 0.04, 0.20, 0.08, 0.01]
+conversion_rates = [0.05, 0.13, 0.09, 0.16, 0.11, 0.04, 0.20, 0.08, 0.01]
 X = np.array(np.zeros([N, d]))
 for i in range(N):
     for j in range(d):
@@ -22,7 +22,7 @@ for i in range(N):
 
 
 #Implementacion de la Seleccion Aleatoria y el Muestreo de Thompson
-# Listas donde guardamos las estrategias seleccionadas
+# Listas donde guardamos las estrategias seleccionadash
 strategies_selected_rs = []  # Aleatorio
 strategies_selected_ts = []  # Thompson
 
@@ -52,16 +52,22 @@ for n in range(0, N):
             strategy_ts = i
     reward_ts = X[n, strategy_ts] 
     if reward_ts == 1:
-        number_of_rewards_1[strategies_selected_ts] += 1 
+        number_of_rewards_1[strategy_ts] += 1 
     else:
-        number_of_rewards_0[strategies_selected_ts] += 1 
+        number_of_rewards_0[strategy_ts] += 1 
     strategies_selected_ts.append(strategy_ts)
     total_reward_ts += reward_ts
 
+absolute_return = (total_reward_ts - total_reward_rs) * 100
+relative_return = (total_reward_ts - total_reward_rs) / total_reward_rs * 100
 
-plt.imshow(X, aspect='auto', cmap='coolwarm')
-plt.title("Simulación de conversiones de usuarios")
-plt.xlabel("Estrategia")
-plt.ylabel("Usuario")
-plt.colorbar(label="Conversión (0 = No, 1 = Sí)")
+print("Rendimiento Absoluto: {:.0f} $".format(absolute_return))
+print("Rendimiento Relativo: {:.0f} %".format(relative_return))
+
+
+# Representación del histograma de selecciones
+plt.hist(strategies_selected_ts)
+plt.title('Histograma de Selecciones')
+plt.xlabel('Estrategia')
+plt.ylabel('Numero de veces que se ha seleccionado la estrategia')
 plt.show()
